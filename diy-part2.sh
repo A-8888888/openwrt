ea5d1c3ab0f9fbd2379269bac94671f025e7ca0a
京A·8888888 <108852509+A-8888888@users.noreply.github.com>
2026-09-18 17:10:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,20 +4,19 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
 # 默认主机名
 sed -i "s/hostname 'OpenWrt'/hostname 'PC-20250616FAXN'/" package/base-files/files/etc/config/system
 
 # 定制登录横幅
 cat > package/base-files/files/etc/banner <<'EOF'
-北冥有鱼 · 定制版 OpenWrt
+北冥有鱼 · 定制版 ImmortalWrt
 =========================================
-  _   _ ____  _   _  ___ _   _   _   _
- | \ | |  _ \| | | |/ _ \ | | | | \ | |
- |  \| | | | | | | | | | | | | |  \| |
- | |\  | |_| | |_| | |_| | |_| | |\  |
- |_| \_|____/ \___/ \___/ \___/ |_| \_|
-=========================================
-  OpenWrt 24.10-SNAPSHOT · AX3000T
-  北冥有鱼定制版 · UA3F + HomeProxy + mwan3
+  ImmortalWrt 24.10 · AX3000T
+  北冥有鱼定制版 · UA3F + mwan3
 EOF
